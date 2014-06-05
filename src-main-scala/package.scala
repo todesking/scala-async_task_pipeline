e21f2791c4630e9fc77fec9430185f14b2c3aadd
todesking <discommunicative@gmail.com>
2014-06-05 19:58:49 +0900

    compile passed

--- src-main-scala/package.scala
+++ src-main-scala/package.scala
@@ -1,12 +1,44 @@
 package com.todesking.async_task_pipeline
 
 import java.util.{concurrent => jc}
+import scala.collection.{mutable => mutable}
 
-trait ExecutionContext
+object AsyncTaskPipeline {
+  val builder = Builder
+}
+
+object Builder {
+  def pipe[A, B]() = new PipeB[A, B]
+
+  class PipeB[A, B] {
+    def unordered(proc:A => Option[B]):Pipe[A, B] = {
+      new UnorderdPipeImpl[A, B](
+        proc,
+        ThreadPoolConfig.default()
+      )
+    }
+    def unordered() = new Unique
+
+    class Unique {
+      def unique[G](groupOf:A => G)(proc:A => Option[B]) = new UnorderedUniquePipeImpl(
+        groupOf, proc,
+        10,
+        ThreadPoolConfig.default()
+      )
+    }
+  }
+}
+
+trait ExecutionContext {
+  // Close pipe and await all process finished
+  def await():Unit
+}
+
 object ExecutionContext {
   def nullSink[A]():SinkExecutionContext[A] =
     new SinkExecutionContext[A]() {
       def feed(value:A):Unit = ()
+      def await():Unit = ()
     }
 }
 
@@ -24,6 +56,15 @@ trait Sink[A] extends Closure {
 trait SourceExecutionContext[A] extends ExecutionContext {
   def wireTo(sink:SinkExecutionContext[A]):Unit
 }
+abstract class SourceExecutionContextImpl[A] extends SourceExecutionContext[A] {
+  var wiredSink:SinkExecutionContext[A] = ExecutionContext.nullSink()
+  override def wireTo(sink:SinkExecutionContext[A]):Unit = {
+    if(wiredSink != null)
+      throw new RuntimeException("Ahhhhhggggg")
+    wiredSink = sink
+  }
+}
+
 trait Source[A] extends Closure {
   def >>(sink:Sink[A]):Closure
   def >>[B](pipe:Pipe[A, B]):Source[B]
@@ -50,6 +91,10 @@ class ConnectPipePipe[A, B, C] (
 
     override def feed(v:A):Unit = leftContext.feed(v)
     override def wireTo(sink:SinkExecutionContext[C]):Unit = rightContext.wireTo(sink)
+    override def await():Unit = {
+      leftContext.await()
+      rightContext.await()
+    }
   }
 }
 
@@ -64,67 +109,105 @@ class ConnectPipeSink[A, B](
     val rightContext = rhs.run()
     leftContext.wireTo(rightContext)
 
-    def feed(v:A):Unit = leftContext.feed(v)
+    override def feed(v:A):Unit = leftContext.feed(v)
+    override def await():Unit = {
+      leftContext.await()
+      rightContext.await()
+    }
   }
 }
 
-class UnorderdPipeImpl[A, B](
-  val proc:(A => B),
-  val queueSize:Int,
-  val corePoolSize:Int,
-  val maxPoolSize:Int,
-  val keepAliveMillis:Int
-) extends Pipe[A, B] {
-  def run() = new Ctx()
-
-  class Ctx extends PipeExecutionContext[A, B] {
-    val threadPool = new BlockingThreadPoolExecutor(
+case class ThreadPoolConfig(
+  queueSize:Int,
+  corePoolSize:Int,
+  maxPoolSize:Int,
+  keepAliveMillis:Int
+) {
+  def createThreadPool() =
+    new BlockingThreadPoolExecutor(
       corePoolSize, maxPoolSize,
       keepAliveMillis,
       new jc.ArrayBlockingQueue[Runnable](queueSize)
     )
+}
 
-    var wiredSink:SinkExecutionContext[B] = ExecutionContext.nullSink()
+object ThreadPoolConfig {
+  def default():ThreadPoolConfig = ThreadPoolConfig(
+    queueSize = 10,
+    corePoolSize = 1,
+    maxPoolSize = 10,
+    keepAliveMillis = 100
+  )
+}
+
+class UnorderdPipeImpl[A, B](
+  val proc:(A => Option[B]),
+  val threadPoolConfig:ThreadPoolConfig
+) extends Pipe[A, B] {
+  override def run() = new Ctx()
+
+  class Ctx extends SourceExecutionContextImpl[B] with PipeExecutionContext[A, B] {
+    val threadPool = threadPoolConfig.createThreadPool()
 
     override def feed(value:A):Unit = {
       threadPool.execute(new Runnable {
         override def run():Unit = {
-          wiredSink.feed(proc(value))
+          proc(value).map(wiredSink.feed(_))
         }
       })
     }
 
-    override def wireTo(sink:SinkExecutionContext[B]):Unit = { this.wiredSink = sink }
+    override def await():Unit = {
+      threadPool.shutdown()
+      while(!threadPool.awaitTermination(100, jc.TimeUnit.MILLISECONDS)) ();
+    }
   }
 }
 
-class UnorderedGroupedPipeImpl[A, B, G](
-  val queueSizePerGroup:Int,
-  val maxProcessItems:Int,
-  val queueSize:Int,
-  val corePoolSize:Int,
-  val maxPoolSize:Int,
-  val keepAliveMillis:Int
-) extends Pipe[A, B] {
-  def run() = new Ctx()
-
-  class Ctx extends PipeExecutionContext[A, B] {
-    val threadPool = new BlockingThreadPoolExecutor(
-      corePoolSize, maxPoolSize,
-      keepAliveMillis,
-      new jc.ArrayBlockingQueue[Runnable](maxPoolSize)
-    )
-    val queue:jc.ArrayBlockingQueue[jc.ArrayBlockingQueue[s]]
-
-    var wiredSink:SinkExecutionContext[B] = ExecutionContext.nullSink()
+// A Pipe such that ∀a:A -> current_execution_count(groupOf(A)) <= 1
+class UnorderedUniquePipeImpl[A, B, G](
+  val groupOf:(A => G),
+  proc:(A => Option[B]),
+  val retryIntervalMillis:Int,
+  threadPoolConfig:ThreadPoolConfig
+) extends UnorderdPipeImpl[A, B](proc, threadPoolConfig) {
+  class Ctx extends SourceExecutionContextImpl[B] with PipeExecutionContext[A, B] {
+    val threadPool = threadPoolConfig.createThreadPool()
+    val processing = new mutable.HashSet[G]
+    private[this] val lock:AnyRef = this
 
     override def feed(value:A):Unit = {
+      val g = groupOf(value)
+
+      var retry = true
+      while(retry) {
+        lock.synchronized {
+          if(!processing.contains(g)) {
+            retry = false
+            processing.add(g)
+          }
+        }
+        if(retry) Thread.sleep(retryIntervalMillis)
+      }
+
+      // At here, well ensured that current_execution_count(g) == 0
+      threadPool.execute(new Runnable {
+        override def run():Unit = {
+          val result = proc(value)
+          lock.synchronized { processing.remove(g) }
+          result.map(wiredSink.feed(_))
+        }
+      })
     }
 
-    override def wireTo(sink:SinkExecutionContext[B]):Unit = { this.wiredSink = sink }
+    override def await():Unit = {
+      while(!processing.isEmpty) Thread.sleep(100)
+
+      threadPool.shutdown
+      while(!threadPool.awaitTermination(100, jc.TimeUnit.MILLISECONDS)) ();
+    }
   }
 }
-
 
 
 class BlockingThreadPoolExecutor(corePoolSize:Int, maxPoolSize:Int, keepAliveMillis:Int, queue:jc.BlockingQueue[Runnable])
@@ -135,6 +218,8 @@ class BlockingThreadPoolExecutor(corePoolSize:Int, maxPoolSize:Int, keepAliveMil
     jc.Executors.defaultThreadFactory,
     new jc.RejectedExecutionHandler() {
       override def rejectedExecution(r:Runnable, executor:jc.ThreadPoolExecutor):Unit = {
+        if(executor.isShutdown)
+          throw new jc.RejectedExecutionException
         executor.getQueue().put(r)
       }
     }
@@ -143,41 +228,29 @@ class BlockingThreadPoolExecutor(corePoolSize:Int, maxPoolSize:Int, keepAliveMil
 
 
 object Main {
+  import java.net.URL
   def main(args:Array[String]):Unit = {
-    /*
-    val src:TraversableOnce[URL]
+    val src:TraversableOnce[URL] = Seq()
 
-    val pipe = Pipeline.builder
+    val builder = AsyncTaskPipeline.builder
 
-    val filterByRule = Pipe.unordered[URL, URL] {url => if(canCrawl(url)) emit(url) }
-    val filterByRobotsTxt = Pipe.unordered[URL, URL].grouped[Domain] {(domain, urls, out) =>
-      def findRule(domain:Domain):RobotsTxt = {
-        RobtsTxtCache.findOrRead(domain)
-      }
-      val robots = findRule(domain)
-      urls.foreach(url => if(robots.canCrawl(url)) out.emit(url))
-    }
-    val throttle = Pipe.unordered.grouped[URL, URL, Domain] {(domain, urls) =>
-      urls.foreach {url =>
-        DomainStatus.waitUntilCrawlable(domain)
-        emit(url)
-      }
-    }
-    val crawl = Pipe.unordered.grouped[URL, PageContent, Domain] {(domain, urls, out) =>
-      def httpGet(url) = { DomainStatus.crawling(domain) { rawGet(url) } }
-      urls.foreach {url=> out.emit(new PageContent(httpGet(url))) }
-    }
-    val persistContent = Pipe.tap[PageContent](repository[PageContent].update(_))
+    type Domain = String
+    def domainOf(url:URL):Domain = ""
 
-    val analyze = Pipeline.unordered[PageContent, PageKeyword](analyze(_))
+    def crawlable(url:URL):Boolean = true
 
-    val persistKeyword = Pipe.tap[PageKeyword](save(_))
+    type PageContent = String
+    def fetch(url:URL):PageContent = ""
 
-    val pipeline:UnorderdPipeline[URL, PageContent] = filterByRule >> filterByRobotsTxt >> throttle >> crawl >> persistContent|(analyze >> persistKeyword)
+    val filterByRule = builder.pipe[URL, URL].unordered {url => Some(url)}
+    val filterByRobtsTxt = builder.pipe[URL, URL].unordered.unique(domainOf(_)) {url => if(crawlable(url)) Some(url) else None }
+    val crawl = builder.pipe[URL, PageContent].unordered.unique(domainOf(_)) {url => Some(fetch(url)) }
 
-    pipeline.emit(src)
-    pipeline.await()
-    */
+    val activePipe = (filterByRule >> filterByRobtsTxt >> crawl).run
+
+    src.foreach(activePipe.feed(_))
+
+    activePipe.await()
   }
 }
 
