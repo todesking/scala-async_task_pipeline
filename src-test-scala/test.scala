1a7ead982ccd018a8c6d0340afc21b494a340abc
todesking <discommunicative@gmail.com>
2014-06-05 21:27:37 +0900

    more test

--- src-test-scala/test.scala
+++ src-test-scala/test.scala
@@ -28,9 +28,45 @@ class Spec extends FlatSpec with Matchers {
 
     val ctx = (builder.pipe[Int, Int].unordered {i => if(i % 2 == 0) Some(i) else None } >> sink).run()
 
-    (1 to 1000).foreach(ctx.feed(_))
+    (1 to 2000).foreach(ctx.feed(_))
     ctx.await()
 
-    results.sorted should be((1 to 1000).filter(_ % 2 == 0))
+    results.sorted should be((1 to 2000).filter(_ % 2 == 0))
+  }
+
+  "UnorderedUniquePipeImpl[A]" should "process values" in {
+    val results = new mutable.ArrayBuffer[Int]
+    val sink = builder.sinkToGrowable(results)
+
+    val ctx = (builder.pipe[Int, Int].unordered.unique(i => i / 3) {i => if(i % 2 == 0) Some(i) else None } >> sink).run()
+
+    (1 to 2000).foreach(ctx.feed(_))
+    ctx.await()
+
+    results.sorted should be((1 to 2000).filter(_ % 2 == 0))
+  }
+
+  "Complex pipeline" should "process values" in {
+    val results = new mutable.ArrayBuffer[Int]
+    val sink = builder.sinkToGrowable(results)
+
+    val ctx = (
+      builder.pipe[String, Int].unordered(i => try {Some(i.toInt)} catch { case _:NumberFormatException => None })
+      >>
+      builder.pipe[Int, String].unordered(i => Some(i.toString))
+      >>
+      builder.pipe[String, Int].unordered(i => Some(i.toInt))
+      >>
+      builder.pipe[Int, Int].unordered.unique(_ % 2) {i => Some(i * 2)}
+      >>
+      sink
+    ).run()
+
+    (1 to 1000).foreach(i => ctx.feed(i.toString))
+    Seq("a", "b", "c").foreach(ctx.feed(_))
+    (1001 to 2000).foreach(i => ctx.feed(i.toString))
+    ctx.await()
+
+    results.sorted should be((1 to 2000).map(_ * 2))
   }
 }
